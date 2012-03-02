ddc75906e214714c4507e561bcdccc487c55da2f
kahlkfhlkaj <kahlkfhlkaj@mailinator.com>
2012-03-02 23:08:40 +0100

    the first commit

---
+++ extension.js
@@ -0,0 +1,33 @@
+
+const St = imports.gi.St;
+const Main = imports.ui.main;
+
+let button;
+
+function _showOverview() {
+    Main.overview.toggle();
+}
+
+function init() {
+    button = new St.Bin({ style_class: 'panel-button',
+                          reactive: true,
+                          can_focus: true,
+                          x_fill: true,
+                          y_fill: false,
+                          track_hover: true });
+    let icon = new St.Icon({ icon_name: 'system-run',
+                             icon_type: St.IconType.SYMBOLIC,
+                             style_class: 'system-status-icon' });
+
+    button.set_child(icon);
+    button.connect('button-press-event', _showOverview);
+    button.connect('enter-event', _showOverview);
+}
+
+function enable() {
+    Main.panel._rightBox.insert_actor(button, Main.panel._rightBox.get_children().length);
+}
+
+function disable() {
+    Main.panel._rightBox.remove_actor(button);
+}
